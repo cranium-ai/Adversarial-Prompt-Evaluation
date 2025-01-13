c607c439be3b1100e5bc5759c5130b2390fcbe36
Ubuntu <ubuntu@ip-10-208-0-239.ec2.internal>
2025-01-13 18:44:13 +0000

    updated training script and dataset

--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -2,9 +2,9 @@
 
 python3 main_n_gram_classifier_train.py
 
-python3 main_classification_fine_tuning.py --model_name bert --model_path_load bert-base-cased
+python3 main_classification_fine_tuning.py --model_name bert --model_name_or_path bert
 
-python3 main_classification_fine_tuning.py --model_name deberta --model_path_load microsoft/deberta-v3-base
+python3 main_classification_fine_tuning.py --model_name deberta --model_name_or_path deberta
 
-python3 main_classification_fine_tuning.py --model_name gpt2 --model_path_load gpt2
+python3 main_classification_fine_tuning.py --model_name gpt2 --model_name_or_path gpt2
 
